18640bac2a811b91f7d3a3a76f0e70303be52630
Christopher Lamm <chris@theantichris.com>
2020-01-15 07:29:27 -0500

    starting user detail page

---
+++ src/routes/users/[username].svelte
@@ -0,0 +1,14 @@
+<script>
+  export async function preload(page) {
+    const { username } = page.params
+
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    const data = await response.json()
+
+    if (response.status === 404) {
+      this.error(404, 'Not found')
+    }
+
+    return { data }
+  }
+</script>
